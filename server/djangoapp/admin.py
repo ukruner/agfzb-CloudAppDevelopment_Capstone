406766ebb7a15f7c6404a6ff870023ec267d7858
ukruner <urmas.kryner@gmail.com>
2024-02-03 08:34:14 -0500

    admin request created, but need to test

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CarModel, CarMake
+from .models import CarModel, CarMake, CarReview
+import requests
 
 
 # Register your models here.
@@ -16,5 +17,34 @@ class CarMakeAdmin(admin.ModelAdmin):
     fields = ["name", "country_origin", "desc"]
     inlines = [CarModelInline]
 
+class CarReviewAdmin(admin.ModelAdmin):
+    actions = ['register_model_from_cloudant']
+    def register_model_from_cloudant(modeladmin, request, queryset):
+        # Check if 'id' is provided in request.GET
+        if 'dealership' not in request.GET:
+            modeladmin.message_user(request, "Please provide an 'id' parameter.", level='error')
+            return
+
+        # Getting the 'id' from request.GET
+        provided_id = request.GET['dealership']
+
+        # Making a call to the cloud database using 'id' as a query parameter
+        api_url = 'https://*.cognitiveclass.ai/api/review'
+        params = {'dealership': provided_id}
+        response = requests.get(api_url, params=params)
+
+        if response.status_code == 200:
+            # Fetched data from cloudant
+            cloud_data = response.json()
+            for data in cloud_data:
+                one_instance = CarReview(dealership=data['dealership'], name=data['name'], id=data['id'], review=data['review'], purchase=data['purchase'], purchase_date=data['purchase_date'], car_make=data['car_make'], car_model=data['car_model'], car_year=data['car_year'])
+                one_instance.save()
+            modeladmin.message_user(request, "Models registered successfully from cloudant.")
+        else:
+            modeladmin.message_user(request, "Error fetching data from cloudant.", level='error')
+
+    register_model_from_cloudant.short_description = "Register review models from cloud cloudant database"
+
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarReview, CarReviewAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -70,3 +69,39 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class CarReview(models.Model):
+    dealership = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True)
+    id = models.IntegerField(blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    purchase = models.BooleanField(blank=True)
+    purchase_date = models.CharField(max_length=30, blank=True)
+    car_make = models.CharField(max_length=50, blank=True)
+    car_model = models.CharField(max_length=50, blank=True)
+    car_year = models.IntegerField(blank=True)
+
+    # def save(self, *args, **kwargs):
+    #     api_url = 'https://*.cognitiveclass.ai/api/review' 
+    #     response = requests.get(api_url)
+    #     if response.status_code == 200:
+    #         models_data = response.json()
+    #         for model_data in models_data:
+    #             dealership = model_data.get('dealership')
+    #             name = model_data.get('name')
+    #             id = model_data.get('id')
+    #             review = model_data.get('review')
+    #             purchase = model_data.get('purchase')
+    #             purchase_date = model_data.get('purchase_date')
+    #             car_make = model_data.get('car_make')
+    #             car_model = model_data.get('car_model')
+    #             car_year = model_data.get('car_year')
+                
+    #             # Creating the CarReview instance
+    #             one_instance = CarReview(dealership=dealership, name=name, id=id, review=review, purchase=purchase, purchase_date=purchase_date, car_make=car_make, car_model=car_model, car_year=car_year)
+    #             one_instance.save()
+    #     else:
+    #         print('Error fetching models from cloud database')
+    #     super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Reviews of the car dealership with id of {str(self.dealership)}: "
